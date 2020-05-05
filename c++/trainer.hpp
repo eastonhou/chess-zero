db821696b79f869fe7c562b156229b6a06f229ba
easton <easton.hou@gmail.com>
2020-05-05 14:18:22 +0800

    .

--- c++/trainer.hpp
+++ c++/trainer.hpp
@@ -19,13 +19,28 @@ public:
         }
     }
 
-    std::list<std::tuple<record_t, target_t>> play() {
+    std::list<std::tuple<record_t, target_t>> play(int nocapture=60) {
         std::list<std::tuple<record_t, target_t>> train_data;
-        std::list<char> captuers;
+        int nocapture_counter = 0;
         auto board = rule_t::initial_board();
         int side = 1;
+        int winner = 0;
         while (!rule_t::gameover_position(board)) {
-            
+            auto result = mcts::ponder(_model, board, side);
+            auto& move = result._0;
+            auto& probs = result._1;
+            auto record = std::make_tuple(board, side);
+            auto target = std::make_tuple(probs, 0);
+            std::tuple<record_t, target_t> train_record = std::make_tuple(record, target);
+            train_data.push_back(train_record);
+            if (board[move[1]] != ' ') nocapture_counter = 0;
+            else if (++nocapture_counter >= nocapture) break;
+            board = move_t::next_board(board, move);
+            side = -side;
         }
+        if (board.find('K') == std::npos) winner = -1;
+        else if (board.find('k') == std::npos) winner = 1;
+        for (auto& x : train_data) x._1._1 = winner;
+        return train_data;
     }
 };
